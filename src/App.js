cc84c9e58e5c26b3465b91a0ccaf2aa4909b0921
amena <amena.ruet@gmail.com>
2022-04-17 15:45:09 +0600

    navber

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
-import Header from './Pages/About/Shared/Header/Header';
+import Header from './Pages/Home/Header/Header';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import SignIn from './Pages/Login/Sing In/SignIn';
 import Services from './Pages/Services/Services';
 import Blogs from './Pages/Blogs/Blogs';
+import About from './Pages/About/Shared/About/About';
+import NotFound from './Pages/About/Shared/NotFound/NotFound';
 
 function App() {
   return (
@@ -14,9 +16,12 @@ function App() {
 
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/signin' element={<SignIn></SignIn>}></Route>
+        <Route path='/home' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
         <Route path='/services' element={<Services></Services>}></Route>
         <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/signin' element={<SignIn></SignIn>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
      
     </div>
--- src/Pages/About/Shared/Header/Header.js
+++ src/Pages/About/Shared/Header/Header.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+//import logo from '../../../../images/logo.png'
+//import { useAuthState } from 'react-firebase-hooks/auth';
+//import auth from '../../../../firebase.init';
+//import { signOut } from 'firebase/auth';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Header = () => {
     return (
-        <div>
-            header
-        </div>
+        <>
+ 
+  
+ <Navbar collapseOnSelect expand="lg" bg="info" variant="light">
+  <Container>
+  <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    
+    <Nav className="ms-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#about">About</Nav.Link>
+      <Nav.Link href="#services">Services</Nav.Link>
+      <Nav.Link href="#blogs">Blogs</Nav.Link>
+      <Nav.Link href="#signin">Sign In</Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+  </Container>
+</Navbar>
+  
+</>
     );
 };
 
